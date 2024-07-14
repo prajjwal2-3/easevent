e1e40d8474d6ed52f8d3a9bb1f00d40d12ee8866
prajjwal2-3 <121849327+prajjwal2-3@users.noreply.github.com>
2024-07-15 02:48:40 +0530

    added next auth, made signup route, setup db

---
+++ app/api/register/route.ts
@@ -0,0 +1,33 @@
+import { NextResponse } from "next/server";
+import prisma from "@/lib/postgresdb";
+import bcrypt from "bcrypt";
+import { getUserFromDb } from "@/lib/userfunctions";
+export async function POST(req: Request) {
+  try {
+    const body = await req.json();
+     console.log(body)
+    const { email, username, firstName, lastName, password } = body.user;
+
+    if (!email || !password) {
+      return new NextResponse("Missing data", { status: 500 });
+    }
+    const user = await getUserFromDb(email);
+
+    if (user) {
+      return new NextResponse("User Already Exists", { status: 500 });
+    }
+    const hashedPassword = await bcrypt.hash(password, 12);
+    const newUser = await prisma.user.create({
+      data: {
+        email,
+        hashedPassword,
+        firstName,
+        lastName,
+        username,
+      },
+    });
+    return NextResponse.json(newUser);
+  } catch (err) {
+    alert("REGISTER_ERROR" + err);
+  }
+}
---
+++ app/signup/page.tsx
@@ -0,0 +1,10 @@
+import Signup from "@/components/signup/Signup";
+
+
+export default function page() {
+  return (
+    <div className="w-full h-screen  flex justify-center items-center bg-blue-400">
+        <Signup/>
+    </div>
+  )
+}
